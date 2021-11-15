43f74656ec8958b82e34ace295f2e06bf50f85aa
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-15 11:11:25 -0500

    Route Parameter updates

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           <Switch>
             <Route path="/products/:id" component={ProductDetails}></Route>
             <Route path="/products" exact render={props => <Products sortBy="newest" {...props}/>}></Route>
-            <Route path="/posts/:year/:month" exact component={Posts}></Route>
+            <Route path="/posts/:year?/:month?" exact component={Posts}></Route>
             <Route path="/admin" exact component={Dashboard}></Route>
             <Route path="/" exact component={Home}></Route>
           </Switch>
