8d1533925d40f46a05e3a17ef4d2053ea6b67f00
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-15 10:48:20 -0500

    Routing updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import {Route, Switch} from 'react-router-dom';
 import NavBar from "./components/navbar";
+import Home from "./components/home";
+import ProductDetails from "./components/productDetails";
 import Products from "./components/products";
 import Posts from "./components/posts";
-import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
 
